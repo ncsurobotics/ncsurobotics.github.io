b771aaae3cab06ebb681b246808ec08e3e477120
wpkelso <wkelso02@gmail.com>
2024-06-20 16:20:41 -0400

    set up blog structure, make post template, start share button scripts

---
+++ html/blog/scripts/share.js
@@ -0,0 +1,20 @@
+document.getElementById("email").onclick = function () {
+  location.href =
+    "mailto:?subject=AquaPack%20Robotics%20-%20PostTitle&body=Check%20out%20this%20blog%20post%20from%20AquaPack%20Robotics!";
+};
+
+document.getElementById("linkedin").onclick = function () {
+  alert("Sorry, this button doesn't work yet");
+};
+
+document.getElementById("xtwitter").onclick = function () {
+  alert("Sorry, this button doesn't work yet");
+};
+
+document.getElementById("mastodon").onclick = function () {
+  alert("Sorry, this button doesn't work yet");
+};
+
+document.getElementById("linkshare").onclick = function () {
+  alert("Sorry, this button doesn't work yet");
+};
