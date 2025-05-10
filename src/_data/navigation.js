670e3f9fd393575dee4bd57e2125e461410eb46e
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2025-05-10 06:54:28 -0400

    feat (WIP): init new eleventy based site

---
+++ src/_data/navigation.js
@@ -0,0 +1,30 @@
+export default {
+  top: [
+    {
+      text: 'Members',
+      url: '/members/'
+    },
+    {
+      text: 'SeaWolves',
+      url: '/seawolves/'
+    },
+    {
+      text: 'Documentation',
+      url: '/documentation/'
+    },
+    {
+      text: 'Calendar',
+      url: '/calendar/'
+    },
+    {
+      text: 'Sponsors',
+      url: '/sponsors/'
+    },
+  ],
+  bottom: [
+    {
+      text: 'Contact Us',
+      url: '/contact/'
+    },
+  ]
+};
---
+++ src/pages/contact.md
@@ -0,0 +1,11 @@
+---
+title: Contact us
+discover:
+  title: Contact us
+permalink: /contact/index.html
+description: Contact information
+layout: page
+---
+
+{{ personal.address }}
+{{ personal.email }}
