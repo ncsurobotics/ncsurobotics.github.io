252f075c93658388f85a0317794430fef196f18e
madrilene <hola@lenesaile.com>
2025-06-16 13:04:00 +0200

    feat: add submenu opt-in for main navigation

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -68,7 +68,8 @@ export const navigation = {
   ariaTop: 'Main',
   ariaBottom: 'Complementary',
   ariaPlatforms: 'Platforms',
-  drawerNav: true
+  drawerNav: true,
+  subMenu: true
 };
 export const themeSwitch = {
   title: 'Theme',
